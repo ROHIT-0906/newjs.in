a631fb45fb5e1ef04bcb67329cbb64ef2a26b617
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-06-04 07:36:39 +0000

    datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+let name = "Rohit makam" // string
+let age = 18 //number 
+let isFollow = true // boolean 
+let tempreture = null // null value 
+let state; // undefinde caus undefined statement
+// symbole
+
+console.log(typeof "Rohit"); // to check the datatype
