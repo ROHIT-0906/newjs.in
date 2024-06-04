56cfb9184b1b1d1a5af224835cb57bb7139ec2f5
ROHIT-0906 <159066477+ROHIT-0906@users.noreply.github.com>
2024-06-04 07:07:16 +0000

    variable

--- 01_basics/var.js
+++ 01_basics/var.js
@@ -1,1 +1,8 @@
 console.log("Rohit Makam");
+const accountId = 1354678
+let accountEmail = "rohitmakam09@gmail.com"
+var accountPassword = "@rohitgithub123"
+accountCity = "Bombay"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
